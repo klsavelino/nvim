1e40392e70e0dae899ff317240988bdf3ad60f5f
Alicia Avelino <klsavelino@gmail.com>
2024-03-27 20:38:09 -0300

    added copilot

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -33,11 +33,21 @@ return {
 			lspconfig.elixirls.setup({capabilities = capabilities})
 			lspconfig.html.setup({capabilities = capabilities})
 
-			
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+			vim.api.nvim_create_autocmd('LspAttach', {
+				group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+			callback = function(ev)
+				vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+				local opts = {buffer = ev.buf}
+				vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+				vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+				vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+				vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+				vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+				vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+				vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+				vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, opts)
+			end,
+			})
+	end,
+}
 }
